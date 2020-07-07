3ce9a9e3f24537e550deb363c60a09d6e85d1ecd
rafi7050 <rafi15-7050@diu.edu.bd>
2020-07-08 00:35:06 +0600

    first commit

---
+++ Ecommerce/core/models.py
@@ -0,0 +1,221 @@
+from django.conf import settings
+from django.db import models
+from django.db.models import Sum
+from django.shortcuts import reverse
+from django_countries.fields import CountryField
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+from .signals import  object_viewed_signal
+
+# Create your models here.
+CATEGORY_CHOICES = (
+    ('SB', 'Flat'),
+    ('TS', 'Property'),
+    ('SK', 'Furniture'),
+    ('HS', 'Apartment')
+)
+
+LABEL_CHOICES = (
+    ('S', 'sale'),
+    ('N', 'new'),
+    ('P', 'promotion')
+)
+
+ADDRESS_CHOICES = (
+    ('B', 'Billing'),
+    ('S', 'Shipping'),
+)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_total_discount_item_price(self):
+        return self.quantity * self.item.discount_price
+
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
+
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ref_code = models.CharField(max_length=20)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    shipping_address = models.ForeignKey(
+        'BillingAddress', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
+    billing_address = models.ForeignKey(
+        'BillingAddress', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
+    payment = models.ForeignKey(
+        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey(
+        'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+    being_delivered = models.BooleanField(default=False)
+    received = models.BooleanField(default=False)
+    refund_requested = models.BooleanField(default=False)
+    refund_granted = models.BooleanField(default=False)
+
+    '''
+    1. Item added to cart
+    2. Adding a BillingAddress
+    (Failed Checkout)
+    3. Payment
+    4. Being delivered
+    5. Received
+    6. Refunds
+    '''
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        if self.coupon:
+            total -= self.coupon.amount
+        return total
+
+
+class BillingAddress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = 'BillingAddresses'
+
+
+class Payment(models.Model):
+    stripe_charge_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=15)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.code
+
+
+class Refund(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    reason = models.TextField()
+    accepted = models.BooleanField(default=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.pk}"
+
+class ObjectViewed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True)
+    object_id = models.PositiveIntegerField()
+    ip_address = models.CharField(max_length=120, blank=True, null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self, ):
+        return "%s viewed: %s" %(self.content_object, self.timestamp)
+
+    class Meta:
+        ordering = ['-timestamp']
+        verbose_name = 'Object Viewed'
+        verbose_name_plural = 'Objects Viewed'
+
+    def object_viewed_receiver(sender, instance, request, *args, **kwargs):
+        c_type = ContentType.objects.get_for_model(sender)
+        ip_address = None
+        try:
+            ip_address = get_client_ip(request)
+        except:
+            pass
+        new_view_instance = ObjectViewed.objects.create(
+            user=request.user,
+            content_type=c_type,
+            object_id=instance.id,
+            ip_address=ip_address
+        )
+
+    object_viewed_signal.connect(object_viewed_receiver)
+
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    job = models.CharField(max_length=100, verbose_name="Service Name")
+    o_date = models.CharField(max_length=100, verbose_name="Date of Order")
+    d_date = models.CharField(max_length=100, verbose_name="Estimated Completion Date")
+    fn = models.CharField(max_length=100, verbose_name="First name")
+    ln = models.CharField(max_length=100, verbose_name="Last name")
+    ads = models.CharField(max_length=100, verbose_name="Phone")
+    YI = models.CharField(max_length=100, blank=True, null=True, verbose_name="Yearly Income")
+    age = models.CharField(max_length=100,blank=True, null=True, verbose_name="Age")
+
+    status = models.CharField(max_length=100, verbose_name="Current Status")
